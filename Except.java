d29963da4315ed9fe7145d5d448046f052c2d266
pradeepgattamaneni <gattamanenipradeep@gmail.com>
2017-01-10 16:53:58 +0530

    Add files via upload

---
+++ Except.java
@@ -0,0 +1,58 @@
+import java.io.*;
+
+
+
+class Except
+
+{
+   
+ public static void accept()
+   
+ {
+      
+  try{
+    
+    InputStreamReader is=new InputStreamReader(System.in);
+ 
+       BufferedReader br=new BufferedReader(is);
+
+        System.out.println("enter name");
+
+        String name=br.readLine();
+ 
+       }
+  
+      catch(IOException ex)
+
+     {
+   
+ 
+    
+ }
+   
+
+ }
+  
+  
+     public static void display() throws IOException
+
+
+   {
+    
+ System.out.println(name);
+
+  }
+
+	
+public static void main (String[] args)
+
+{
+	
+accept();
+	
+display();
+
+}
+
+
+}
---
+++ student.java
@@ -0,0 +1,36 @@
+class student
+
+{
+
+String[5] name;
+int[5] age;
+
+int elder;
+Scanner s=new Scanner();
+
+    public void accept()
+   
+ {
+   for(int i=0;i<5;i++)
+{
+this.name[i]=s.next();
+this.age[i]=s.nextInt();
+if(elder<age[i])
+{
+elder=age[i];
+}
+
+}
+for(int i=0;i<5;i++)
+{
+if(age[i]==elder)
+{
+System.out.println(name[i]+" is the eldest" );
+}
+
+}
+
+}
+
+
+}
